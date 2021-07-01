d55d1943b5bb698fbf6988f90c37662370845d3b
sinhadip21 <dipsingha2@gmail.com>
2021-07-01 17:36:52 +0600

    Stack Data Structure implementation

--- Stack/Index.java
+++ Stack/Index.java
@@ -1,2 +1,17 @@
-package DS_ALGO.Stack;public class Index {
+package DS_ALGO.Stack;
+
+public class Index {
+    public static void main(String[] args) {
+
+        Stack S1 = new Stack();
+        S1.push(12);
+        S1.push(57);
+        S1.push(78);
+        S1.peek();
+        S1.push(101);
+        S1.pop();
+
+        S1.show();
+
+    }
 }
--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,2 +1,37 @@
-package DS_ALGO.Stack;public class Stack {
+package DS_ALGO.Stack;
+
+public class Stack {
+
+    int stack[] = new int[10];
+    int top = 0;
+
+    // Push data
+    public void push(int data) {
+        stack[top] = data;
+        top++;
+    }
+
+    // Pop data
+    public void pop() {
+//        top--;
+        System.out.println("top is : " + stack[top-1]);
+        stack[top-1] = 0;
+    }
+
+    // Peek
+    public int peek() {
+        int data = stack[top-1];
+        System.out.println("Peek: " + data);
+        return data;
+    }
+
+    // Print data
+    public void show() {
+
+        for(int st : stack) {
+            System.out.println(st);
+        }
+
+    }
+
 }
