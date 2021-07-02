8d1cfcb56b2eab70fff304cc876b6dc0ac78087a
sinhadip21 <dipsingha2@gmail.com>
2021-07-02 14:24:40 +0600

    Added Stack DS all feature and fixed some issu

--- Stack/Index.java
+++ Stack/Index.java
@@ -4,12 +4,29 @@ public class Index {
     public static void main(String[] args) {
 
         Stack S1 = new Stack();
-        S1.push(12);
-        S1.push(57);
-        S1.push(78);
-        S1.peek();
         S1.push(101);
-        S1.pop();
+        S1.push(102);
+        S1.push(103);
+//        S1.push(104);
+//        S1.push(105);
+//        S1.push(106);
+//        S1.push(107);
+//        S1.push(108);
+//        S1.push(109);
+//        S1.push(110);
+//        S1.push(111);
+        S1.peek();
+        S1.size();
+        S1.isEmpty();
+
+//        S1.pop();
+//        S1.pop();
+//        S1.pop();
+//        S1.pop();
+//
+//        S1.peek();
+//        S1.size();
+//        S1.isEmpty();
 
         S1.show();
 
--- Stack/Stack.java
+++ Stack/Stack.java
@@ -7,30 +7,63 @@ public class Stack {
 
     // Push data
     public void push(int data) {
-        stack[top] = data;
-        top++;
+        if(top==10) {
+            System.out.println("\nSTACK IS FULL");
+        }else{
+            stack[top] = data;
+            top++;
+        }
     }
 
     // Pop data
     public void pop() {
-//        top--;
-        System.out.println("top is : " + stack[top-1]);
-        stack[top-1] = 0;
+        if(isEmpty()) {
+            System.out.println("\nSTACK IS EMPTY");
+        }else{
+            top--;
+            System.out.println("POP : " + stack[top]);
+            stack[top] = 0;
+        }
+
     }
 
     // Peek
     public int peek() {
-        int data = stack[top-1];
-        System.out.println("Peek: " + data);
+        int data = 0;
+        if(top <= 0) {
+            System.out.println("\nSTACK IS EMPTY, PEEK : 0");
+        }else {
+            data = stack[top-1];
+            System.out.println("PEEK : " + data);
+        }
         return data;
+    }
+
+    // Size
+    public int size() {
+        System.out.println("Size : " + top);
+        return top;
+    }
+
+    // Is empty
+    public boolean isEmpty() {
+        boolean res;
+        if(top <= 0) {
+            res = true;
+        } else {
+            res = false;
+        }
+        System.out.println("Is empty : " + res);
+        return res;
     }
 
     // Print data
     public void show() {
 
         for(int st : stack) {
-            System.out.println(st);
+            System.out.print(st+"  ");
         }
+        System.out.println();
 
     }
 
